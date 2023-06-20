aa9fdca1f6c57cae3a813852961001afcada18a8
Kristoffer Follestad <kristoffer@bonsay.no>
2023-06-20 18:42:35 +0200

    Refactored error-handler

--- ErrorHandler.php
+++ ErrorHandler.php
@@ -5,8 +5,6 @@ namespace Rammewerk\Component\ErrorHandler;
 use Closure;
 use Throwable;
 use ErrorException;
-use JetBrains\PhpStorm\Pure;
-use JetBrains\PhpStorm\NoReturn;
 
 class ErrorHandler {
 
@@ -17,8 +15,6 @@ class ErrorHandler {
     private array $logCallbacks = [];
 
 
-
-
     /**
      * ErrorHandler Constructor
      *
@@ -26,35 +22,33 @@ class ErrorHandler {
      *
      */
     public function __construct() {
-        error_reporting( E_ALL );
-        ini_set( 'display_errors', 'Off' );
-        set_error_handler( [ $this, 'handleError' ], E_ALL );
-        set_exception_handler( [ $this, 'handleException' ] );
-        register_shutdown_function( [ $this, 'handleShutdown' ] );
+        error_reporting(E_ALL);
+        ini_set('display_errors', 'Off');
+        set_error_handler([$this, 'handleError'], E_ALL);
+        set_exception_handler([$this, 'handleException']);
+        register_shutdown_function([$this, 'handleShutdown']);
     }
-
-
 
 
     /**
      * Handle an uncaught exception from the application.
      *
-     * @param \Throwable $e
+     * @param Throwable $e
+     *
+     * @return never
      */
-    #[NoReturn] public function handleException(Throwable $e): void {
+    public function handleException(Throwable $e): never {
 
         foreach( $this->logCallbacks as $callback ) {
-            $callback( $e );
+            $callback($e);
         }
 
         foreach( $this->reportCallbacks as $callback ) {
-            $callback( $e );
+            $callback($e);
         }
 
         die;
     }
-
-
 
 
     /**
@@ -65,27 +59,23 @@ class ErrorHandler {
      * @param string $file
      * @param int $line
      *
-     * @throws \ErrorException
+     * @return bool
+     * @throws ErrorException
      */
-    public function handleError(int $level, string $message, string $file = '', int $line = 0): void {
-        if( $level && error_reporting() ) {
-            throw new ErrorException( $message, 0, $level, $file, $line );
-        }
+    public function handleError(int $level, string $message, string $file = '', int $line = 0): bool {
+        if( !( $level && error_reporting() ) ) return false;
+        throw new ErrorException($message, 0, $level, $file, $line);
     }
-
-
 
 
     /**
      * Handle the PHP shutdown event.
      */
     public function handleShutdown(): void {
-        if( ! is_null( $error = error_get_last() ) && $this->isFatal( $error['type'] ) ) {
-            $this->handleException( $this->fatalExceptionFromError( $error ) );
+        if( !is_null($error = error_get_last()) && $this->isFatal($error['type']) ) {
+            $this->handleException($this->fatalExceptionFromError($error));
         }
     }
-
-
 
 
     /**
@@ -96,30 +86,26 @@ class ErrorHandler {
      * @return bool
      */
     private function isFatal(int $type): bool {
-        return in_array( $type, [ E_COMPILE_ERROR, E_CORE_ERROR, E_ERROR, E_PARSE ], true );
+        return in_array($type, [E_COMPILE_ERROR, E_CORE_ERROR, E_ERROR, E_PARSE], true);
     }
-
-
 
 
     /**
      * Fatal exception from error
      *
-     * @param array $error
+     * @param array{type:int,message:string,file:string,line:int} $error
      *
-     * @return \ErrorException
+     * @return ErrorException
      */
-    #[Pure] private function fatalExceptionFromError(array $error): ErrorException {
-        return new ErrorException( $error['message'], $error['type'], 0, $error['file'], $error['line'] );
+    private function fatalExceptionFromError(array $error): ErrorException {
+        return new ErrorException($error['message'], $error['type'], 0, $error['file'], $error['line']);
     }
-
-
 
 
     /**
      * Register the exception handling callbacks for the application.
      *
-     * @param \Closure $closure
+     * @param Closure $closure
      * @param bool $reset will reset all previous log closures if set to true
      *
      * @return void
@@ -130,12 +116,10 @@ class ErrorHandler {
     }
 
 
-
-
     /**
      * Register the exception handling callbacks for the application.
      *
-     * @param \Closure $closure
+     * @param Closure(Throwable):void $closure
      * @param bool $reset will reset all previous log closures if set to true
      *
      * @return void
@@ -144,6 +128,5 @@ class ErrorHandler {
         if( $reset ) $this->reportCallbacks = [];
         $this->reportCallbacks[] = $closure;
     }
-
 
 }
